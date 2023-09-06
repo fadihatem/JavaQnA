989fbdb8bee88943befe7973fc264a456138f7b2
fadihatem <fadster2003@yahoo.ca>
2023-09-05 20:40:28 -0400

    Add hops

---
+++ src/javaQnA/Hops.java
@@ -0,0 +1,61 @@
+package javaQnA;
+
+import java.io.IOException;
+import java.util.Arrays;
+
+public class Hops {
+	public long getSecondsRequired(long N, int F, long[] P) {
+		// Write your code here
+		long minValue = Arrays.stream(P).min().getAsLong();
+		long maxValue = Arrays.stream(P).max().getAsLong();
+		if (N < 2 || N > 1000000000000L || F < 1 || F > 500000L || minValue < 1 || maxValue > N - 1) {
+			return 0;
+		}
+		long[] lilyPads = new long[(int) N];
+		for (int i = 0; i < P.length; i++) {
+			lilyPads[(int) P[i]] = 1;
+		}
+		int jumps = 1;
+		for (int i = 0; i < P.length; i++) {
+			int startIndex = -1;
+			if (P[i] > 0) {
+				startIndex = i;
+				++jumps;
+			}
+			while (startIndex > -1 && startIndex < P.length) {
+				if (P[startIndex] == 0) {
+					++jumps;
+				}
+				++startIndex;
+			}
+		}
+		return jumps;
+	}
+
+	// These are the tests we use to determine if the solution is correct.
+	// You can add your own at the bottom.
+	int test_case_number = 1;
+
+	void check(long expected, long output) {
+		boolean result = (expected == output);
+		char rightTick = '\u2713';
+		char wrongTick = '\u2717';
+		if (result) {
+			System.out.println(rightTick + " Test #" + test_case_number);
+		} else {
+			System.out.print(wrongTick + " Test #" + test_case_number + ": Expected " + expected);
+			System.out.print(" Your output: " + output);
+			System.out.println();
+		}
+		test_case_number++;
+	}
+
+	private void run() {
+		check(2, getSecondsRequired(3, 1, new long[] { 1 }));
+		check(4, getSecondsRequired(6, 3, new long[] { 5, 2, 4 }));
+	}
+
+	public static void main(String[] args) throws IOException {
+		new Hops().run();
+	}
+}
